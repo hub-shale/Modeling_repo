05d4a62b120dd524f36de33c963671388c9830a3
hub-shale <shale@bren.ucsb.edu>
2022-04-07 13:04:38 -0700

    start

---
+++ Almond_model.R
@@ -0,0 +1,20 @@
+#' Almond model
+#' 
+#' data is the data.frame used as input data
+#' p is the precipitation coefficient
+#' p2 is the precipitation squared coefficient
+#' t is the temperature coefficient
+#' t2 is the temperature squared coefficient
+#' 
+#' 
+#' 
+
+aanomaly = function(data, p, p2, t, t2, intercept) {
+  
+  
+  return(value)
+}
+
+
+# Testing
+aanomaly(data = )
